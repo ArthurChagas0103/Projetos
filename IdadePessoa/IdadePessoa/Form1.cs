ca47c56d21e0e16a80b0fdff513938a82fbe328b
ArthurChagas0103 <86752180+ArthurChagas0103@users.noreply.github.com>
2022-03-13 23:01:54 -0300

    13/03/2022

---
+++ IdadePessoa/IdadePessoa/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IdadePessoa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int anoNascimento, anoAtual, idadeAnos, idadeMeses, idadeSemanas, idadeDias;
+
+            anoNascimento = int.Parse(txtAno.Text);
+            idadeAnos = int.Parse(lblAnos.Text);
+            idadeMeses = int.Parse(lblMeses.Text);
+            idadeSemanas = int.Parse(lblSemanas.Text);
+            idadeDias = int.Parse(lblDias.Text);
+
+            anoAtual = DateTime.Now.Year;
+            idadeAnos = anoAtual - anoNascimento;
+            idadeMeses = idadeAnos * 12;
+            idadeSemanas = idadeAnos * 52;
+            idadeDias = idadeAnos * 365;
+
+            lblAnos.Text = idadeAnos.ToString();
+            lblMeses.Text = idadeMeses.ToString();
+            lblSemanas.Text = idadeSemanas.ToString();
+            lblDias.Text = idadeDias.ToString();
+        }
+    }
+}
