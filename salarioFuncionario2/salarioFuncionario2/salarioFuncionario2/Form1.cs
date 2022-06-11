d2fcb4cf441ae37c24fcd43c67ecb3d4f4635cf9
ArthurChagas0103 <86752180+ArthurChagas0103@users.noreply.github.com>
2022-06-11 13:04:28 -0300

    11/06/2022

---
+++ salarioFuncionario2/salarioFuncionario2/salarioFuncionario2/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace salarioFuncionario2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int cod;
+            double inss, salarioBruto, horasTrabalhadas, horasExtras, horasExtras2, valorHorasExtras, salarioLiquido, salarioHorasExtras;
+
+            cod = int.Parse(txtCodigoMatricula.Text);
+            horasTrabalhadas = double.Parse(txtHorasTrabalhadas.Text);
+            horasExtras = double.Parse(txtHorasExtras.Text);
+
+            cod = cod / 10000;
+
+            lblClasse.Text = cod.ToString();
+
+            switch (cod.ToString()){
+                case "1": 
+                    salarioBruto = horasTrabalhadas * 8;
+                    lblSalarioBruto.Text = "R$ " + salarioBruto.ToString();
+                    /*---------------------------------------------------*/
+                    valorHorasExtras = (horasExtras * 8);
+                    horasExtras2 = (valorHorasExtras / 2) + valorHorasExtras;
+                    lblHorasExtras.Text = "R$ " + horasExtras2.ToString();
+                    /*---------------------------------------------------*/
+                    salarioHorasExtras = salarioBruto + horasExtras2;
+                    lblSalarioHorasExtras.Text = "R$ " + salarioHorasExtras.ToString();
+                    /*---------------------------------------------------*/
+                    inss = (11 * salarioBruto) / 100;
+                    lblINSS.Text = "R$ " + inss.ToString();
+                    /*---------------------------------------------------*/
+                    salarioLiquido = salarioBruto + horasExtras2 - inss;
+                    lblSalarioLiquido.Text = "R$ " + salarioLiquido.ToString();
+                    break;
+                case "2":
+                    salarioBruto = horasTrabalhadas * 10;
+                    lblSalarioBruto.Text = "R$ " + salarioBruto.ToString();
+                    /*---------------------------------------------------*/
+                    valorHorasExtras = (horasExtras * 10);
+                    horasExtras2 = (valorHorasExtras / 2) + valorHorasExtras;
+                    lblHorasExtras.Text = "R$ " + horasExtras2.ToString();
+                    /*---------------------------------------------------*/
+                    salarioHorasExtras = salarioBruto + horasExtras2;
+                    lblSalarioHorasExtras.Text = "R$ " + salarioHorasExtras.ToString();
+                    /*---------------------------------------------------*/
+                    inss = (11 * salarioBruto) / 100;
+                    lblINSS.Text = "R$ " + inss.ToString();
+                    /*---------------------------------------------------*/
+                    salarioLiquido = salarioBruto + horasExtras2 - inss;
+                    lblSalarioLiquido.Text = "R$ " + salarioLiquido.ToString();
+                    break;
+                case "3":
+                    salarioBruto = horasTrabalhadas * 12;
+                    lblSalarioBruto.Text = "R$ " + salarioBruto.ToString();
+                    /*---------------------------------------------------*/
+                    valorHorasExtras = (horasExtras * 12);
+                    horasExtras2 = (valorHorasExtras / 2) + valorHorasExtras;
+                    lblHorasExtras.Text = "R$ " + horasExtras2.ToString();
+                    /*---------------------------------------------------*/
+                    salarioHorasExtras = salarioBruto + horasExtras2;
+                    lblSalarioHorasExtras.Text = "R$ " + salarioHorasExtras.ToString();
+                    /*---------------------------------------------------*/
+                    inss = (11 * salarioBruto) / 100;
+                    lblINSS.Text = "R$ " + inss.ToString();
+                    /*---------------------------------------------------*/
+                    salarioLiquido = salarioBruto + horasExtras2 - inss;
+                    lblSalarioLiquido.Text = "R$ " + salarioLiquido.ToString();
+                    break;
+                case "4":
+                    salarioBruto = horasTrabalhadas * 15;
+                    lblSalarioBruto.Text = "R$ " + salarioBruto.ToString();
+                    /*---------------------------------------------------*/
+                    valorHorasExtras = (horasExtras * 15);
+                    horasExtras2 = (valorHorasExtras / 2) + valorHorasExtras;
+                    lblHorasExtras.Text = "R$ " + horasExtras2.ToString();
+                    /*---------------------------------------------------*/
+                    salarioHorasExtras = salarioBruto + horasExtras2;
+                    lblSalarioHorasExtras.Text = "R$ " + salarioHorasExtras.ToString();
+                    /*---------------------------------------------------*/
+                    inss = (11 * salarioBruto) / 100;
+                    lblINSS.Text = "R$ " + inss.ToString();
+                    /*---------------------------------------------------*/
+                    salarioLiquido = salarioBruto + horasExtras2 - inss;
+                    lblSalarioLiquido.Text = "R$ " + salarioLiquido.ToString();
+                    break;
+                case "5":
+                    salarioBruto = horasTrabalhadas * 20;
+                    lblSalarioBruto.Text = "R$ " + salarioBruto.ToString();
+                    /*---------------------------------------------------*/
+                    valorHorasExtras = (horasExtras * 20);
+                    horasExtras2 = (valorHorasExtras / 2) + valorHorasExtras;
+                    lblHorasExtras.Text = "R$ " + horasExtras2.ToString();
+                    /*---------------------------------------------------*/
+                    salarioHorasExtras = salarioBruto + horasExtras2;
+                    lblSalarioHorasExtras.Text = "R$ " + salarioHorasExtras.ToString();
+                    /*---------------------------------------------------*/
+                    inss = (11 * salarioBruto) / 100;
+                    lblINSS.Text = "R$ " + inss.ToString();
+                    /*---------------------------------------------------*/
+                    salarioLiquido = salarioBruto + horasExtras2 - inss;
+                    lblSalarioLiquido.Text = "R$ " + salarioLiquido.ToString();
+                    break;
+                default: MessageBox.Show("Código inválido", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Information);break;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtCodigoMatricula.Text = "";
+            txtHorasTrabalhadas.Text = "";
+            txtHorasExtras.Text = "";
+            lblClasse.Text = "1/2/3/4/5";
+            lblSalarioBruto.Text = "R$";
+            lblHorasExtras.Text = "R$";
+            lblSalarioHorasExtras.Text = "R$";
+            lblINSS.Text = "R$";
+            lblSalarioLiquido.Text = "R$";
+        }
+    }
+}
