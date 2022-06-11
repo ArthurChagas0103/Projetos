78c5c4664957359246c694312e83ab83017cee4e
ArthurChagas0103 <86752180+ArthurChagas0103@users.noreply.github.com>
2022-06-11 13:31:27 -0300

    Add files via upload

---
+++ C#/CadastroCliente/CadastroCliente/Form3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroCliente
+{
+    public partial class formLoginSenha : Form
+    {
+        public formLoginSenha()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            if(txtUsuario.Text == "")
+            {
+                MessageBox.Show("Por favor, digite um usuário!", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (txtSenha.Text == "")
+            {
+                MessageBox.Show("Por favor, digite uma senha!", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (txtUsuario.Text == "Arthur Chagas" && txtSenha.Text == "1234")
+            {
+                var form = new formUsuariosCadastrados();
+                form.Show();
+                txtUsuario.Text = "";
+                txtSenha.Text = "";
+                Close();
+            }
+            else { 
+                MessageBox.Show("Usuário ou Senha inválidos!", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void checkMostrarSenha_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkMostrarSenha.Checked)
+            {
+                txtSenha.UseSystemPasswordChar = true;
+            }
+            else
+            {
+                txtSenha.UseSystemPasswordChar = false;
+            }
+        }
+    }
+}
---
+++ C#/Calc_Cofrinho/Calc_Cofrinho/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Calc_Cofrinho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1Cent = new System.Windows.Forms.TextBox();
+            this.txt5Cent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt10Cent = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt25Cent = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt50Cent = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt1Real = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(354, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Piggy Bank Calculator";
+            // 
+            // txt1Cent
+            // 
+            this.txt1Cent.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1Cent.Location = new System.Drawing.Point(28, 98);
+            this.txt1Cent.Multiline = true;
+            this.txt1Cent.Name = "txt1Cent";
+            this.txt1Cent.Size = new System.Drawing.Size(93, 33);
+            this.txt1Cent.TabIndex = 1;
+            this.txt1Cent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt5Cent
+            // 
+            this.txt5Cent.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt5Cent.Location = new System.Drawing.Point(163, 98);
+            this.txt5Cent.Multiline = true;
+            this.txt5Cent.Name = "txt5Cent";
+            this.txt5Cent.Size = new System.Drawing.Size(93, 33);
+            this.txt5Cent.TabIndex = 5;
+            this.txt5Cent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "1 Centavo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(160, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "5 Centavos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(295, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "10 Centavos";
+            // 
+            // txt10Cent
+            // 
+            this.txt10Cent.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt10Cent.Location = new System.Drawing.Point(298, 98);
+            this.txt10Cent.Multiline = true;
+            this.txt10Cent.Name = "txt10Cent";
+            this.txt10Cent.Size = new System.Drawing.Size(93, 33);
+            this.txt10Cent.TabIndex = 10;
+            this.txt10Cent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(430, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "25 Centavos";
+            // 
+            // txt25Cent
+            // 
+            this.txt25Cent.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt25Cent.Location = new System.Drawing.Point(433, 98);
+            this.txt25Cent.Multiline = true;
+            this.txt25Cent.Name = "txt25Cent";
+            this.txt25Cent.Size = new System.Drawing.Size(93, 33);
+            this.txt25Cent.TabIndex = 12;
+            this.txt25Cent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(565, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 18);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "50 Centavos";
+            // 
+            // txt50Cent
+            // 
+            this.txt50Cent.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt50Cent.Location = new System.Drawing.Point(568, 98);
+            this.txt50Cent.Multiline = true;
+            this.txt50Cent.Name = "txt50Cent";
+            this.txt50Cent.Size = new System.Drawing.Size(93, 33);
+            this.txt50Cent.TabIndex = 14;
+            this.txt50Cent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(700, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 18);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "1 Real";
+            // 
+            // txt1Real
+            // 
+            this.txt1Real.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1Real.Location = new System.Drawing.Point(703, 98);
+            this.txt1Real.Multiline = true;
+            this.txt1Real.Name = "txt1Real";
+            this.txt1Real.Size = new System.Drawing.Size(93, 33);
+            this.txt1Real.TabIndex = 16;
+            this.txt1Real.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(127, 99);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 32);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "+";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(262, 99);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 32);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "+";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(397, 99);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(30, 32);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "+";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(532, 99);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(30, 32);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "+";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(667, 99);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(30, 32);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "+";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(802, 99);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(30, 32);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "=";
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.Location = new System.Drawing.Point(881, 98);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(29, 32);
+            this.lblValor.TabIndex = 23;
+            this.lblValor.Text = "0";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(838, 99);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(49, 32);
+            this.label15.TabIndex = 24;
+            this.label15.Text = "R$";
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(403, 169);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(98, 43);
+            this.btnSomar.TabIndex = 25;
+            this.btnSomar.Text = "SOMAR";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(507, 169);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(98, 43);
+            this.btnLimpar.TabIndex = 26;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 231);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt1Real);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt50Cent);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt25Cent);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt10Cent);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt5Cent);
+            this.Controls.Add(this.txt1Cent);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Piggy Bank Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1Cent;
+        private System.Windows.Forms.TextBox txt5Cent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt10Cent;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt25Cent;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt50Cent;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt1Real;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
