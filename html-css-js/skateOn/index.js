b05747d4e93fd0ed27d1c23286c4217ab2c775ef
ArthurChagas0103 <arthurgchagas@gmail.com>
2022-08-12 02:19:45 -0300

    12/08/2022

--- html-css-js/skateOn/index.js
+++ html-css-js/skateOn/index.js
@@ -106,3 +106,42 @@ function abrirTela7(){
 function abrirTela8(){
 	window.open("produtos.html","_self");
 }
+
+var TRange = null;
+
+function findString(str) {
+	alert('Continue apertando a lupa para ir para próximos resultados')
+	if (parseInt(navigator.appVersion) < 4) return;
+	var strFound;
+	if (window.find) {
+
+		// CODE FOR BROWSERS THAT SUPPORT window.find
+
+		strFound = self.find(str);
+		if (!strFound) {
+			strFound = self.find(str, 0, 1);
+			while (self.find(str, 0, 1)) continue;
+		}
+	}
+	else if (navigator.appName.indexOf("Microsoft") != -1) {
+
+		// EXPLORER-SPECIFIC CODE
+
+		if (TRange != null) {
+			TRange.collapse(false);
+			strFound = TRange.findText(str);
+			if (strFound) TRange.select();
+		}
+		if (TRange == null || strFound == 0) {
+			TRange = self.document.body.createTextRange();
+			strFound = TRange.findText(str);
+			if (strFound) TRange.select();
+		}
+	}
+	else if (navigator.appName == "Opera") {
+		alert("Opera browsers not supported, sorry...")
+		return;
+	}
+	if (!strFound) alert("String '" + str + "' not found!")
+	return;
+}
