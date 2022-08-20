40b5d43465096be85b199fcc07d6fdde6024ba46
ArthurChagas0103 <86752180+ArthurChagas0103@users.noreply.github.com>
2022-08-19 23:10:59 -0300

    19/08/2022

---
+++ C#/operacoesMatematicas/operacoesMatematicas/Metodos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operacoesMatematicas
+{
+    internal class Metodos
+    {
+        public double Soma(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtracao(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiplicacao(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divisao(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ C#/operacoesMatematicas/operacoesMatematicas/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operacoesMatematicas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sair = "", operacao;
+            double num1, num2, resultado;
+
+            Metodos operacoes = new Metodos();
+
+            while (sair != "s")
+            {
+                Console.Write("Digite o primeiro número: ");
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Digite o segundo número: ");
+                num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("");
+
+                Console.Write("Digite a operação desejada: ");
+                operacao = Console.ReadLine().ToUpper();
+
+                Console.WriteLine("");
+
+                if (operacao == "SOMA")
+                {
+                    resultado = operacoes.Soma(num1, num2);
+                    Console.Write("O resultado da soma é: " + resultado);
+                    Console.WriteLine("");
+                }
+                else if (operacao == "SUBTRAÇÃO")
+                {
+                    resultado = operacoes.Subtracao(num1, num2);
+                    Console.Write("O resultado da subtração é: " + resultado);
+                    Console.WriteLine("");
+                }
+                else if (operacao == "MULTIPLICAÇÃO")
+                {
+                    resultado = operacoes.Multiplicacao(num1, num2);
+                    Console.Write("O resultado da multiplicação é: " + resultado);
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    resultado = operacoes.Divisao(num1, num2);
+                    Console.Write("O resultado da divisão é: " + resultado);
+                    Console.WriteLine("");
+                }
+
+                Console.WriteLine("");
+
+                Console.Write("Digite 's' para sair ou 'c' para continuar: ");
+                sair = Console.ReadLine();
+
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
