536de53d6f214f207e64deb3e789fda01d85c1e6
ArthurChagas0103 <86752180+ArthurChagas0103@users.noreply.github.com>
2022-09-02 02:39:18 -0300

    02/09/2022

---
+++ C#/numeroDeDivisores/numeroDeDivisores/Classe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroDeDivisores
+{
+    internal class Classe
+    {
+        public int Divisores(int valorDigitado)
+        {
+            int i, contador = 0;
+
+            for (i = 1; i <= valorDigitado; i++)
+            {
+                if (valorDigitado % i == 0)
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+
+        public string Primo(int valorDigitado)
+        {
+            Classe objeto = new Classe();
+
+            if(objeto.Divisores(valorDigitado) == 2)
+            {
+                return "Sim";
+            }
+            else
+            {
+                return "Não";
+            }
+            
+        }
+    }
+}
---
+++ C#/numeroDeDivisores/numeroDeDivisores/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroDeDivisores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numDigitado;
+            string sair = "";
+
+            while(sair.ToLower() != "s")
+            {
+                Classe objeto = new Classe();
+
+                Console.Write("Digite um número: ");
+                    numDigitado = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Número de divisores de " + numDigitado + ": " + objeto.Divisores(numDigitado));
+                Console.WriteLine("O número é primo: " + objeto.Primo(numDigitado));
+
+                Console.WriteLine("");
+
+                Console.Write("Digite 'c' para continuar ou 's' para sair: ");
+                    sair = Console.ReadLine();
+
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+ /* Descobrir quais são os divisores do número
+int numDigitado = 0, i;
+
+Console.Write("Digite um número: ");
+numDigitado = int.Parse(Console.ReadLine());
+
+for (i = 1; i <= numDigitado; i++)
+{
+    if (numDigitado % i == 0)
+    {
+        Console.WriteLine("Divisor de " + numDigitado + ": " + i);
+    }
+}
+Console.ReadKey();
+*/
