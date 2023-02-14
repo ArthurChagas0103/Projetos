c540b21579ea40e1864ad00ac3644e20645559da
ArthurChagas0103 <arthurgchagas@gmail.com>
2023-02-14 14:20:08 -0300

    14/02/2023

--- js/Projeto01/Questao06/js/index.js
+++ js/Projeto01/Questao06/js/index.js
@@ -1,3 +1,17 @@
-$( "#select" ).change(function() {
-    $('#iframe').attr('src', "../Questao01/index.html");
+$("#select").change(function() {
+    if($("#select").val() == 1){
+        $('#iframe').attr('src', "../Questao01/index.html");
+    }
+    else if($("#select").val() == 2){
+        $('#iframe').attr('src', "../Questao02/index.html");
+    }
+    else if($("#select").val() == 3){
+        $('#iframe').attr('src', "../Questao03/index.html");
+    }
+    else if($("#select").val() == 4){
+        $('#iframe').attr('src', "../Questao04/index.html");
+    }
+    else if($("#select").val() == 5){
+        $('#iframe').attr('src', "../Questao05/index.html");
+    }
 });
