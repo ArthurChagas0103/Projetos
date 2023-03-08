30198d04a114e1ea669fdfec11af6d3c82a7c640
ArthurChagas0103 <arthurgchagas@gmail.com>
2023-03-07 22:46:07 -0300

    03/07/2023

--- js/Projeto03/Questao02/js/script.js
+++ js/Projeto03/Questao02/js/script.js
@@ -1,3 +1,18 @@
-localStorage.getItem("arrayQuestao1");
+let getLocalStorage = localStorage.getItem("arrayQuestao1");
 
-console.log(arrayQuestao1)
+let arrayQuestao1 = [];
+
+arrayQuestao1 = getLocalStorage.split(",");
+
+var arrayNumerico = arrayQuestao1.map(Number);
+
+console.log(arrayNumerico)
+
+let procuraNumero = parseInt(prompt("Digite o número que gostaria de procurar: "))
+
+if(arrayNumerico.indexOf(procuraNumero) != -1){
+    console.log("Número encontrado!")
+}
+else{
+    console.log("Número não encontrado!")
+}
