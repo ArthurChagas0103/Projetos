f8ee5cfa481607821ebe8fe16d1965fe19c5db08
ArthurChagas0103 <arthurgchagas@gmail.com>
2023-04-11 09:10:48 -0300

    11/04/2023

--- js/Projeto05/ESTOQUE-TEMPOEXECUÇAO/script.js
+++ js/Projeto05/ESTOQUE-TEMPOEXECUÇAO/script.js
@@ -1,25 +1,29 @@
-
 let btnEnviar = document.querySelectorAll('#botoes button')[0];
 let btnExcluir = document.querySelectorAll('#botoes button')[1];
 let btnEditar = document.querySelectorAll('#botoes button')[2];
-
 let nome = document.querySelectorAll('#wrap input')[0];
 let quantidade = document.querySelectorAll('#wrap input')[1];
 let preco = document.querySelectorAll('#wrap input')[2];
-
 let prateleira = document.querySelectorAll('#wrap input')[3];
 let descricao = document.querySelectorAll('#wrap input')[4];
 let categoria = document.querySelectorAll('#wrap input')[5];
-
 let tabela = document.querySelector('#saida table');
 let BD = [];
 
+function limpar() {
+    nome.value = "";
+    quantidade.value = "";
+    preco.value = "";
+    prateleira.value = "";
+    descricao.value = "";
+    categoria.value = "";
+}
 
 //btnEnviar.addEventListener('click', cadastrar)
 
 //function cadastrar(){
 //métodos
-btnEnviar.onclick = function(){
+btnEnviar.onclick = function () {
 
     let produto = new Object();
     produto.nome = nome.value;
@@ -28,53 +32,59 @@ btnEnviar.onclick = function(){
     produto.prateleira = prateleira.value;
     produto.descricao = descricao.value;
     produto.categoria = categoria.value;
-    
     produto.id = BD.length;
-    
-    BD.push(produto);
-    tabela.innerHTML += `<tr><td><input type="checkbox" id="${produto.id}" onchange="verificar(this.id)"></td><td>${produto.nome}</td><td>${produto.quantidade}</td><td>${preco.value}</td></tr>`;
-//}
+
+    if (produto.nome != "" && produto.quantidade != "" && produto.preco != "" && produto.prateleira != "" && produto.descricao != "" && produto.categoria) {
+        BD.push(produto);
+        tabela.innerHTML += `<tr><td><input type="checkbox" id="${produto.id}" onchange="verificar(this.id)"></td><td>${produto.nome}</td><td>${produto.quantidade}</td><td>${produto.preco}</td><td>${produto.prateleira}</td><td>${produto.descricao}</td><td>${produto.categoria}</td></tr>`;
+        limpar();
+    }
+    else {
+        alert('Complete todos os campos!')
+    }
+    //}
 }
-btnExcluir.onclick = function(){
-    for (let i = 0; i < BD.length; i++){
+btnExcluir.onclick = function () {
+    for (let i = 0; i < BD.length; i++) {
         let elemento = document.querySelectorAll('#saida table tr td input')[i];
-        if (elemento.checked){
+        if (elemento.checked) {
             BD.splice(elemento.id, 1);
-            tabela.innerHTML = `<tr><td width="30px"></td><td>Nome</td><td>Quant.</td><td>Preço</td></tr>`;
+            tabela.innerHTML = `<tr><td width="30px"></td><td>Nome</td><td>Quant.</td><td>Preço</td><td>Prateleira</td><td>Descricao</td><td>Categoria</td></tr>`;
             montarTabela();
         }
     }
+    limpar();
 }
 
-function montarTabela(){
-    for (let i = 0; i < BD.length; i++){
-        tabela.innerHTML += `<tr><td width="30px"><input type="checkbox" id="${i}" onchange="verificar()"></td><td>${BD[i].nome}</td><td>${BD[i].quantidade}</td><td>${BD[i].preco}</td></tr>`;
+function montarTabela() {
+    for (let i = 0; i < BD.length; i++) {
+        tabela.innerHTML += `<tr><td width="30px"><input type="checkbox" id="${i}" onchange="verificar()"></td><td>${BD[i].nome}</td><td>${BD[i].quantidade}</td><td>${BD[i].preco}</td><td>${BD[i].prateleira}</td><td>${BD[i].descricao}</td><td>${BD[i].categoria}</td></tr>`;
     }
 }
 
-btnEditar.onclick = function(){
-    for (let i = 0; i < BD.length; i++){
+btnEditar.onclick = function () {
+    for (let i = 0; i < BD.length; i++) {
         let elemento = document.querySelectorAll('#saida table tr td input')[i];
-        if (elemento.checked){
+        if (elemento.checked) {
             BD[i].nome = nome.value;
             BD[i].quantidade = quantidade.value;
             BD[i].preco = preco.value;
-
             BD[i].prateleira = prateleira.value;
             BD[i].descricao = descricao.value;
             BD[i].categoria = categoria.value;
 
-            tabela.innerHTML = `<tr><td width="30px"></td><td>Nome</td><td>Quant.</td><td>Preço</td></tr>`;
+            tabela.innerHTML = `<tr><td width="30px"></td><td>Nome</td><td>Quant.</td><td>Preço</td><td>Prateleira</td><td>Descrição</td><td>Categoria</td></tr>`;
             montarTabela();
         }
-    }    
+    }
+    limpar();
 }
 
-function verificar(id){
+function verificar(id) {
     let cont = 0;
-    for (let i = 0; i < BD.length; i++){
+    for (let i = 0; i < BD.length; i++) {
         let elemento = document.querySelectorAll('#saida table tr td input')[i];
-        if (elemento.checked){
+        if (elemento.checked) {
             nome.value = BD[i].nome;
             quantidade.value = BD[i].quantidade;
             preco.value = BD[i].preco;
@@ -83,7 +93,7 @@ function verificar(id){
             categoria.value = BD[i].categoria;
 
             cont++;
-            if (cont > 1){
+            if (cont > 1) {
                 alert('Não é possível selecionar mais de 1 elemento.');
                 elemento.checked = false;
                 break;
