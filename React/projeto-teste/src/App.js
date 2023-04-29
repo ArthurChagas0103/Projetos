0bfe7e745de8444ee71783338131a30f5a986b5d
ArthurChagas0103 <arthurgchagas@gmail.com>
2023-04-29 19:44:52 -0300

    29/04/2023

---
+++ React/projeto-teste/src/App.js
@@ -0,0 +1,12 @@
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+    </div>
+  );
+}
+
+export default App;
