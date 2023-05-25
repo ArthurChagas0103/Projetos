ce761ff20f8b6f4710a06329dee6038000e4544f
ArthurChagas0103 <arthurgchagas@gmail.com>
2023-05-25 10:57:13 -0300

    25/05/2023

--- Typescript/projeto-01-typescript/dist/index.js
+++ Typescript/projeto-01-typescript/dist/index.js
@@ -1,34 +1,29 @@
-let nome: string = "";
-let idade: number = 0;
-let disciplina: string = "";
-let especializacao: string = "";
-let empresa: string = "";
-let salario: number = 0;
-
+"use strict";
+let nome = "";
+let idade = 0;
+let disciplina = "";
+let especializacao = "";
+let empresa = "";
+let salario = 0;
 nome = "Arthur Gomes Chagas";
 idade = 18;
 disciplina = "Desenvolvimento Web";
-especializacao = "Engenharia de Software";
+especializacao = "Desenvolvimento Web e Mobile";
 empresa = "Google";
-salario = 20000.00
-
+salario = 20000.00;
 console.log(`Me chamo ${nome} e tenho ${idade} anos. Estudo no Colégio Cotemig e faço a disciplina de ${disciplina}. Estou iniciando na carreira de programação e gostaria de me especializar em ${especializacao}. Quando eu formar, pretendo trabalha na empresa ${empresa} e ter um salário de R$${salario}!`);
-
-let numeros: number[] = [1, 2, 3, 4, 5];
-
+let numeros = [1, 2, 3, 4, 5];
 for (let chave in numeros) {
     console.log("chave =", chave);
 }
-
 for (let valor of numeros) {
     console.log("valor =", valor);
 }
-
 let valor = 1000;
 while (valor > 0) {
     console.log("Valor =", valor);
     valor -= 100;
 }
-
+//npx tsc
+//nod .\dist\index.js
 //${} ou ,
-// break quebra da repetição toda, continue pula um certo momento da repetição, por exemplo, ir de 1 a 5 mas pular o 2
